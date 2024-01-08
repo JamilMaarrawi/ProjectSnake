b92c699cc7df7c9248492d71ac31aeb2904f67c0
Jamil Maarrawi <jamil.maarrawi@stud.fh-campuswien.ac.at>
2024-01-08 19:54:10 +0100

    DONE!!!

--- src/main/java/at/ac/fhcampuswien/apple.java
+++ src/main/java/at/ac/fhcampuswien/apple.java
@@ -3,17 +3,20 @@ package at.ac.fhcampuswien;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import Snake.*;
 
-public class apple {
+public class Apple {
     GamePanel gp;
     public BufferedImage i = null;
     public int x, y;
-    int count = 0;
-    public apple(GamePanel gp){
+    public Apple(GamePanel gp){
         this.gp = gp;
         getImage();
-        this.x = getX() * gp.TileSize;
-        this.y = getY() * gp.TileSize;
+        while (true) {
+            this.x = getX() * gp.TileSize;
+            this.y = getY() * gp.TileSize;
+            if(CheckIfValid(x/gp.TileSize,y/gp.TileSize)) break;
+        }
     }
     public void getImage(){
         int x = 1 + (int)(Math.random() * ((3 - 1) + 1));
@@ -26,14 +29,25 @@ public class apple {
         }
     }
     public int getX(){
-        int x = 1 + (int)(Math.random() * ((21 - 1) + 1));
+        int x = 1 + (int)(Math.random() * ((20 - 1) + 1));
         return x;
     }
     public int getY(){
-        int y = 5 + (int)(Math.random() * ((22 - 5) + 1));
+        int y = 5 + (int)(Math.random() * ((21 - 5) + 1));
         return y;
     }
     public void draw(Graphics2D g2){
         g2.drawImage(i,x,y,gp.TileSize,gp.TileSize,null);
+    }
+    public boolean CheckIfValid(int x, int y){
+        boolean Valid=true;
+        if(gp.TM.map[y][x]!=0) Valid=false;
+        for (Snake s : gp.w){
+            if (s.x / gp.TileSize == x && s.y / gp.TileSize == y) {
+                Valid = false;
+                break;
+            }
+        }
+        return Valid;
     }
 }
